e886fc991617a1985169487f866e0142677b1375
Wojtek J <wojtekj1989@gmail.com>
2020-01-06 17:23:57 +0100

    Test login form

--- app/src/androidTest/java/com/example/beerlab/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/beerlab/LoginActivityTest.java
@@ -1,20 +1,24 @@
 package com.example.beerlab;
 
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.filters.LargeTest;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 
 @RunWith(AndroidJUnit4ClassRunner.class)
-@LargeTest
 public class LoginActivityTest {
 
     @Rule
@@ -38,4 +42,42 @@ public class LoginActivityTest {
         onView(withId(R.id.login_actv_password_id)).check(matches(isDisplayed()));
         onView(withId(R.id.button_login_id)).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void test_loginWithEmptyForm() {
+        onView(withId(R.id.button_login_id)).perform(click());
+
+        onView(withId(R.id.login)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_loginWithInvalidEmailFormat() {
+        ViewInteraction loginText = onView(withId(R.id.login_act_email_id));
+        loginText.perform(typeText("notAnEmail"));
+        loginText.check(matches(withText("notAnEmail")));
+        loginText.check(matches(hasErrorText("Invalid email format")));
+    }
+
+    @Test
+    public void test_registerClick() {
+        ViewInteraction registerButton = onView(withId(R.id.textView_registerHere));
+        registerButton.check(matches(isDisplayed()));
+        registerButton.perform(click());
+        onView(withId(R.id.register)).check(matches(isDisplayed()));
+    }
+
+//    @Test
+//    public void test_validLogin() throws Exception{
+//        server = new MockWebServer();
+//        server.enqueue(new MockResponse()
+//                        .setResponseCode(200)
+//                        .setBody("OK"));
+//        // TODO: use server.url() in the request call
+//        server.start();
+//        onView(withId(R.id.login_act_email_id)).perform(typeText("valid@email.com"));
+//        onView(withId(R.id.login_actv_password_id)).perform(typeText("password"));
+//        onView(withId(R.id.button_login_id)).perform(click());
+//
+//        onView(withId(R.id.menu)).check(matches(isDisplayed()));
+//    }
 }
