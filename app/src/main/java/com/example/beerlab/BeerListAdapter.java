c688c05ab62723c2c8bff902cd9dc06e8f62bb37
Nezale <nezale456@gmail.com>
2020-01-16 22:47:17 +0100

    Fixed menu

--- app/src/main/java/com/example/beerlab/BeerListAdapter.java
+++ app/src/main/java/com/example/beerlab/BeerListAdapter.java
@@ -14,23 +14,23 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+public class BeerListAdapter extends RecyclerView.Adapter<BeerListAdapter.BeerListViewHolder> {
     private Context mContext;
     private ArrayList<ExampleItem> mExampleList;
 
-    public ExampleAdapter(Context context, ArrayList<ExampleItem> exampleList) {
-        mContext = context;
+    public BeerListAdapter(MenuFragment context, ArrayList<ExampleItem> exampleList) {
+        mContext = context.getActivity();
         mExampleList = exampleList;
     }
 
     @Override
-    public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(mContext).inflate(R.layout.example_item, parent, false);
-        return new ExampleViewHolder(v);
+    public BeerListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.beer_list_item, parent, false);
+        return new BeerListViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ExampleViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull BeerListViewHolder holder, int position) {
         ExampleItem currentItem = mExampleList.get(position);
 
         String imageUrl = currentItem.getmImageUrl();
@@ -49,18 +49,27 @@ public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleV
         return mExampleList.size();
     }
 
-    public class ExampleViewHolder extends RecyclerView.ViewHolder {
+    public class BeerListViewHolder extends RecyclerView.ViewHolder implements  View.OnClickListener {
+
         public ImageView mImageView;
         public TextView mTextViewBeer;
         public TextView mTextViewDescription;
         public TextView mTextViewPrice;
 
-        public ExampleViewHolder(@NonNull View itemView) {
+        public BeerListViewHolder(@NonNull View itemView) {
             super(itemView);
             mImageView = itemView.findViewById(R.id.imageView_menu);
             mTextViewBeer = itemView.findViewById(R.id.textView_beerName);
             mTextViewDescription = itemView.findViewById(R.id.textView_description);
             mTextViewPrice = itemView.findViewById(R.id.textView_price);
+            itemView.setOnClickListener(this);
         }
+
+        public void onClick(View view){
+
+        }
+
     }
+
+
 }
