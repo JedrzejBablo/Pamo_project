804bbbde605d8eedd1aaf8b884a0ddd315779ec0
Nezale <nezale456@gmail.com>
2020-01-21 23:21:22 +0100

    Fixed quiz

--- app/src/main/java/com/example/beerlab/view/QuizFragment.java
+++ app/src/main/java/com/example/beerlab/view/QuizFragment.java
@@ -1,5 +1,6 @@
 package com.example.beerlab.view;
 
+import android.app.Application;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.beerlab.Questions;
@@ -23,20 +25,19 @@ public class QuizFragment extends Fragment {
     private String mAnswer;
     public int mScore = 0;
     private int mQuestionLength = mQuestions.mQuestions.length;
+    private Button answer1, answer2, answer3, answer4;
+    AppCompatActivity appCompatActivity = new AppCompatActivity();
+    private TextView score, question;
+
+    private int r;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_quiz, container, false);
+        final View view = inflater.inflate(R.layout.fragment_quiz, container, false);
 
-        Button answer1, answer2, answer3, answer4;
 
-        TextView score, question;
 
-        int r;
-
-        protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            final View view = inflater.inflate(R.layout.fragment_quiz,container,false)
 
             r = 0;
 
@@ -107,75 +108,79 @@ public class QuizFragment extends Fragment {
                     }
                 }
             });
-        }
 
-        private void updateQuestion(int num) {
-            if (num > 5) {
-                endTheGame();
-            } else {
-                question.setText(mQuestions.getQuestion(num));
-                answer1.setText(mQuestions.getChoice1(num));
-                answer2.setText(mQuestions.getChoice2(num));
-                answer3.setText(mQuestions.getChoice3(num));
-                answer4.setText(mQuestions.getChoice4(num));
 
-                mAnswer = mQuestions.getCorrectAnwer(num);
-            }
-        }
 
-        private void gameOver() {
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(QuizActivity.this);
-            alertDialogBuilder
-                    .setMessage("Zła odpowiedz udało Ci się uzyskać  " + mScore + " " + "punktów.")
-                    .setCancelable(false)
-                    .setPositiveButton("Zacznij od nowa",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    startActivity(new Intent(getApplicationContext(), QuizActivity.class));
-                                    finish();
-                                }
-                            })
-
-                    .setNegativeButton("Wyjscie",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    Toast.makeText(QuizActivity.this, "", Toast.LENGTH_LONG).show();
-                                    finish();
-                                }
-                            });
-
-            AlertDialog alertDialog = alertDialogBuilder.create();
-            alertDialog.show();
-        }
-
-        private void endTheGame() {
-            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(QuizActivity.this);
-            alertDialogBuilder
-                    .setMessage("Gratulacje uzyskałeś  " + mScore + " " + "punktów.")
-                    .setCancelable(false)
-                    .setPositiveButton("Zacznij od nowa",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    startActivity(new Intent(getApplicationContext(), QuizActivity.class));
-                                    finish();
-                                }
-                            })
-
-                    .setNegativeButton("Wyjscie",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    Toast.makeText(QuizActivity.this, "", Toast.LENGTH_LONG).show();
-                                    finish();
-                                }
-                            });
-
-            AlertDialog alertDialog = alertDialogBuilder.create();
-            alertDialog.show();
-        }
-
+        return view;
     }
+
+    private void updateQuestion(int num) {
+        if (num > 5) {
+            endTheGame();
+        } else {
+            question.setText(mQuestions.getQuestion(num));
+            answer1.setText(mQuestions.getChoice1(num));
+            answer2.setText(mQuestions.getChoice2(num));
+            answer3.setText(mQuestions.getChoice3(num));
+            answer4.setText(mQuestions.getChoice4(num));
+
+            mAnswer = mQuestions.getCorrectAnwer(num);
+        }
+    }
+
+    private void gameOver() {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext().getApplicationContext());
+        alertDialogBuilder
+                .setMessage("Zła odpowiedz udało Ci się uzyskać  " + mScore + " " + "punktów.")
+                .setCancelable(false)
+                .setPositiveButton("Zacznij od nowa",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                startActivity(new Intent(getContext().getApplicationContext(), MenuFragment.class));
+                                appCompatActivity.finish();
+                            }
+                        })
+
+                .setNegativeButton("Wyjscie",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(getContext().getApplicationContext(), "", Toast.LENGTH_LONG).show();
+                                appCompatActivity.finish();
+                            }
+                        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void endTheGame() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext().getApplicationContext());
+        alertDialogBuilder
+                .setMessage("Gratulacje uzyskałeś  " + mScore + " " + "punktów.")
+                .setCancelable(false)
+                .setPositiveButton("Zacznij od nowa",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                startActivity(new Intent(getContext().getApplicationContext(), MenuFragment.class));
+                                appCompatActivity.finish();
+                            }
+                        })
+
+                .setNegativeButton("Wyjscie",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(getContext().getApplicationContext(), "", Toast.LENGTH_LONG).show();
+                                appCompatActivity.finish();
+                            }
+                        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
 }
